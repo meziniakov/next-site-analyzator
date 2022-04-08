22ed29bd9ff483175dba4d31c05e5c7cda6da376
mezin <z2941@ya.ru>
2022-04-08 15:17:11 +0300

    ver 08.04.22

--- pages/project/[_id].js
+++ pages/project/[_id].js
@@ -4,78 +4,96 @@ import { useRouter } from "next/router";
 import Layout from "../../components/admin/layout.admin";
 import { useCallback, useEffect, useState } from "react";
 import { Tabs, Table, Tooltip, Button, message, Space } from "antd";
-import { MailTwoTone } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, MailTwoTone } from "@ant-design/icons";
+import Link from "next/link";
 
 const { TabPane } = Tabs;
 
-const columns = [
-  {
-    title: "Домен",
-    dataIndex: "domain",
-    width: 300,
-    fixed: "left",
-    ellipsis: {
-      showTitle: false,
-    },
-    render: (text, record) => (
-      <Tooltip placement="topLeft" title={record.title}>
-        <A href={record._id} text={text} />
-        <br />
-        <span>{record.title}</span>
-      </Tooltip>
-    ),
-  },
-  {
-    title: "Email",
-    dataIndex: "emails",
-    width: 80,
-    render: (text, record) => (
-      <>
-        {text && record.emails?.length !== 0 ? (
-          <Tooltip title={text}>
-            <MailTwoTone twoToneColor="#1890ff" />
-          </Tooltip>
-        ) : (
-          ""
-        )}
-      </>
-    ),
-  },
-  {
-    title: `TR`,
-    dataIndex: "traffic",
-  },
-  {
-    title: `TS`,
-    dataIndex: "traffic_season",
-  },
-  {
-    title: `PS`,
-    dataIndex: "project_stage",
-  },
-  {
-    title: `Profit`,
-    dataIndex: "profit_await",
-  },
-  {
-    title: `Max цена`,
-    dataIndex: "evaluate_max",
-  },
-  {
-    title: `Ср. цена`,
-    dataIndex: "evaluate_middle",
-  },
-  {
-    title: `Min цена`,
-    dataIndex: "evaluate_min",
-  },
-];
-
 export default function ProjectId({ props }) {
+  const columns = [
+    {
+      title: "Домен",
+      dataIndex: "domain",
+      width: 300,
+      fixed: "left",
+      ellipsis: {
+        showTitle: false,
+      },
+      render: (text, record) => (
+        <Tooltip placement="topLeft" title={record.title}>
+          <A href={record._id} text={text} />
+          <br />
+          <span>{record.title}</span>
+        </Tooltip>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "emails",
+      width: 80,
+      render: (text, record) => (
+        <>
+          {text && record.emails?.length !== 0 ? (
+            <Tooltip title={record.emails.join("\n")}>
+              <MailTwoTone twoToneColor="#1890ff" />
+            </Tooltip>
+          ) : (
+            ""
+          )}
+        </>
+      ),
+    },
+    {
+      title: `TR`,
+      dataIndex: "traffic",
+    },
+    {
+      title: `TS`,
+      dataIndex: "traffic_season",
+    },
+    {
+      title: `PS`,
+      dataIndex: "project_stage",
+    },
+    {
+      title: `Profit`,
+      dataIndex: "profit_await",
+    },
+    {
+      title: `Max цена`,
+      dataIndex: "evaluate_max",
+    },
+    {
+      title: `Ср. цена`,
+      dataIndex: "evaluate_middle",
+    },
+    {
+      title: `Min цена`,
+      dataIndex: "evaluate_min",
+    },
+    {
+      title: "Действия",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Link href={"/"}>
+            <a onClick={(e) => handleDelete(e, record._id)}>
+              <DeleteOutlined />
+            </a>
+          </Link>
+          <A href={"/project/edit/" + record._id} text={<EditOutlined />} />
+        </Space>
+      ),
+    },
+  ];
+
   const router = useRouter();
   const { _id } = router.query;
   const [domains, setDomains] = useState([]);
   const [selectedDomains, setSelectedDomains] = useState([]);
+  const [megaindex, setMegaindex] = useState(false);
+  const [blackList, setBlackList] = useState(false);
+  const [emails, setEmails] = useState(false);
 
   const getDomains = useCallback(() => {
     axios
@@ -86,32 +104,62 @@ export default function ProjectId({ props }) {
       .catch((e) => console.log(e));
   }, [_id]);
 
-  async function onGetEmails(req, res) {
+  const handleDelete = async (e, domainId) => {
+    e.preventDefault();
+    await axios
+      .delete(`${process.env.NEXT_PUBLIC_API_SERVER}/api/domain/${domainId}`)
+      .then((res) => getDomains())
+      .catch((e) => console.log(e));
+  };
+
+  const handleMegaindex = async (e) => {
+    setMegaindex(true);
+    await axios
+      .post(
+        `${process.env.NEXT_PUBLIC_API_SERVER}/api/megaindex/`,
+        selectedDomains
+      )
+      .then((res) => {
+        getDomains();
+        setMegaindex(false);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const handleBlackList = async (e) => {
+    e.preventDefault();
+    setBlackList(true);
+    await axios
+      .post(
+        `${process.env.NEXT_PUBLIC_API_SERVER}/api/domain/blacklist/`,
+        selectedDomains
+      )
+      .then((res) => {
+        getDomains();
+        setBlackList(false);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  async function handleGetEmails() {
+    setEmails(true);
     selectedDomains.forEach(async (el) => {
       await axios
         .post(
           `${process.env.NEXT_PUBLIC_API_SERVER}/api/parser/emails`,
           { domain: el.domain }
-          // {
-          //   headers: {
-          //     "Access-Control-Allow-Origin": "*",
-          //     "Access-Control-Allow-Credentials": "true",
-          //     "Access-Control-Max-Age": "1800",
-          //     "Access-Control-Allow-Headers": "content-type",
-          //     "Access-Control-Allow-Methods":
-          //       "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-          //   },
-          // }
           // AXIOS_OPTIONS
         )
         .then((res) => {
-          console.log(res.data.emails);
-          res.status === "200"
-            ? message.error("Ошибка")
-            : message.success("Успешно");
+          res.data.emails.length > 0
+            ? message.success(
+                `${el.domain}: собрано ${res.data.emails.length} адрес(-ов)`
+              )
+            : message.error(`${el.domain}: не удалось найти :(`);
           getDomains();
+          setEmails(false);
         })
-        .catch((e) => alert(e.message, "error"));
+        .catch((e) => message.error("Error"));
     });
   }
 
@@ -147,11 +195,27 @@ export default function ProjectId({ props }) {
                 </Button>
                 <Button
                   type="primary"
-                  onClick={onGetEmails}
+                  onClick={handleBlackList}
                   disabled={!selectedDomains?.length}
-                  // loading={loading}
+                  loading={blackList}
+                >
+                  В черный список
+                </Button>
+                <Button
+                  type="primary"
+                  onClick={handleGetEmails}
+                  disabled={!selectedDomains?.length}
+                  loading={emails}
                 >
                   Собрать почту
+                </Button>
+                <Button
+                  type="primary"
+                  onClick={handleMegaindex}
+                  disabled={!selectedDomains?.length}
+                  loading={megaindex}
+                >
+                  Megaindex
                 </Button>
               </Space>
               <span style={{ marginLeft: 8 }}>
@@ -162,11 +226,10 @@ export default function ProjectId({ props }) {
               pagination={false}
               rowKey={"_id"}
               scroll={{ y: "70vh", x: 1500 }}
-              rowSelection={rowSelection}
-              // rowSelection={{
-              //   type: selectionType,
-              //   ...rowSelection,
-              // }}
+              // rowSelection={rowSelection}
+              rowSelection={{
+                ...rowSelection,
+              }}
               columns={columns}
               dataSource={domains}
             />
@@ -182,4 +245,4 @@ export default function ProjectId({ props }) {
     </Layout>
   );
 }
-ProjectId.auth = true;
+// ProjectId.auth = true;
